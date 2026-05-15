eef95f26ab19ff644693d32db337cd1ee053966a
Potranco <chemar@gmail.com>
2026-05-15 15:18:49 +0200

    change interface

--- dist/src/DataTables.d.ts
+++ dist/src/DataTables.d.ts
@@ -1,3 +1,3 @@
 import { lang, DatosTabla } from './interfaces';
-declare const getDataTables: (id: number, lang?: lang) => Promise<DatosTabla | Error>;
+declare const getDataTables: (id: number, lang?: lang) => Promise<DatosTabla[] | Error>;
 export { getDataTables };
--- src/DataTables.ts
+++ src/DataTables.ts
@@ -2,7 +2,7 @@ import { getData } from "./getData";
 import type { lang, DatosTabla } from "./interfaces";
 const INE_URL_DATATABLE = 'DATOS_TABLA'
 
-const getDataTables = async (id:number, lang:lang = 'ES'): Promise<DatosTabla | Error> => {
+const getDataTables = async (id:number, lang:lang = 'ES'): Promise<DatosTabla[] | Error> => {
     return getData(lang, INE_URL_DATATABLE, id)
 }
 
